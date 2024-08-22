e87ea14e33df65c1ac5f9fa165f3daafea3cc357
samuel <monteirosamuel159@gmail.com>
2024-08-22 16:18:01 -0300

    Teste

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Contador from './pages/contador/Contador'
+import Tarefa from './pages/tarefa/Tarefa'
+
+function App() {
+
+  return (
+    // <>
+    //     <Home 
+    //     titulo='Minha primeira postagem' 
+    //     texto='Texto da minha primeira postagem' />
+    // </>
+    // <Contador />
+    <Tarefa />
+  )
+}
+
+export default App
---
+++ src/pages/tarefa/Tarefa.tsx
@@ -0,0 +1,27 @@
+﻿import { useEffect, useState } from "react"
+
+function Tarefa() {
+
+  const [completed, setComplete] = useState(false);
+  const [tarefa, setTarefa] = useState('');
+
+  useEffect( () => {
+    if (completed){
+      setTarefa('Parabens! voce terminou')
+    }else{
+      setTarefa('')
+    }
+      
+  }, [completed])
+
+  return (
+    <div>
+      <h2>Componente Tarefa</h2>
+      <h3>{tarefa}</h3>
+      <p>Conclua a tarefa</p>
+      <button onClick={() => setComplete(!completed) }>Concluir Tarefa</button>
+    </div>
+  )
+}
+
+export default Tarefa
